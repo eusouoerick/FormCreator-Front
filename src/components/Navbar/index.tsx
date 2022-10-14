789f3174b6b3b230b7f1fbb8ba548bc8f34cbd0e
Erick Gabriel <limaerick.03@gmail.com>
2022-10-14 01:07:11 -0300

    feat: edit user link and logout icon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ import ShareModal from '../ShareModal';
 import * as S from './styles';
 
 type TypesNavbar = {
-  page?: 'questions' | 'answers' | 'dashboard' | 'creator';
+  page?: 'questions' | 'answers' | 'dashboard' | 'creator' | 'edit';
   blockUser?: number;
 };
 
@@ -26,11 +26,16 @@ const Navbar = ({ page, blockUser = 0 }: TypesNavbar) => {
           <S.StyledLink href='/dashboard' checked={page === 'dashboard'}>
             Dashboard
           </S.StyledLink>
+
+          {(page === 'dashboard' || page === 'edit') && (
+            <S.StyledLink href='/dashboard' checked={page === 'edit'}>
+              Edit user
+            </S.StyledLink>
+          )}
+
           {query.slug && (
             <>
-              <S.StyledLink href={`/forms/${query.slug}`} checked={page === 'questions'}>
-                Questions
-              </S.StyledLink>
+              <S.StyledLink href={``}>Questions</S.StyledLink>
 
               {blockUser === user?.id && (
                 <S.StyledLink
@@ -56,7 +61,7 @@ const Navbar = ({ page, blockUser = 0 }: TypesNavbar) => {
         {user && (
           <S.UserArea>
             <S.UserTextInfo>
-              <span>{user?.name}</span>
+              <span>{user?.name.split(' ')[0]}</span>
               <span className='id'>#{user?.id}</span>
             </S.UserTextInfo>
             <S.UserImage>
@@ -65,12 +70,16 @@ const Navbar = ({ page, blockUser = 0 }: TypesNavbar) => {
                 layout='fill'
                 objectPosition='bottom'
                 objectFit='cover'
-                alt=''
+                alt='user image'
                 height={50}
                 width={50}
                 quality={100}
               />
             </S.UserImage>
+
+            <span className='material-icons' translate='no'>
+              logout
+            </span>
           </S.UserArea>
         )}
       </S.Container>
